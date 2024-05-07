7edfb91fd037f937bbac19f0cbb6eb314c880d58
Leonardo Barbosa Sales <leobarbo@c3r13p5.42sp.org.br>
2024-05-07 14:24:01 -0300

    DIO - Bank Python

--- bank_dio.py
+++ bank_dio.py
@@ -1,51 +1,66 @@
 WITHDRAW_LIMIT = 3
 
-while TRUE:
+while True:
 		print("Welcome to the bank")
 		print("1. Create Account")
 		print("2. Deposit")
 		print("3. Withdraw")
 		print("4. Check Balance")
 		print("5. Exit")
-		choice = input("Enter your choice: ")
 
-		if choice == '1':
-			print("Create Account")
+		try:
+			choice = int(input("Enter your choice: "))
+		except ValueError:
+			print("\nInvalid choice\n")
+			continue
+
+		if choice == 1:
+			print("Create Account\n")
 			name = input("Enter your name: ")
-			account_number = input("Enter your account number: ")
-			balance = input("Enter your balance: ")
-			print("Account created successfully")
+			try:
+				account_number = int(input("Enter your account number: "))
+				balance = int(input("Enter your balance: "))
+			except ValueError:
+				print("\nInvalid input\n")
+				continue
+			print("\nAccount created successfully\n")
 
-		if choice == '2':
+		elif choice == 2:
 			print("Deposit")
-			amount = input("Enter the amount to deposit: ")
+			try:
+				amount = int(input("Enter the amount to deposit: "))
+			except ValueError:
+				print("\nInvalid input\n")
+				continue
 			balance = balance + amount
-			print("Amount deposited successfully")
+			print("Amount deposited successfully\n")
 
-		if choice == '3':
+		elif choice == 3:
 			print("Withdraw")
 			if WITHDRAW_LIMIT == 0:
 				print("You have reached the withdraw limit")
 				continue
-			amount = input("Enter the amount to withdraw: ")
+			try:
+				amount = int(input("Enter the amount to withdraw: "))
+			except ValueError:
+				print("\nInvalid input\n")
+				continue
 			if amount > 500:
-				print("Withdraw limit exceeded")
-			if amount > balance:
-				print("Insufficient balance")
+				print("\nWithdraw limit exceeded\n")
+			elif amount > balance:
+				print("Insufficient balance\n")
 			else:
 				balance = balance - amount
 				WITHDRAW_LIMIT = WITHDRAW_LIMIT - 1
-				print("Amount withdrawn successfully")
+				print("Amount withdrawn successfully\n")
 
-		if choice == '4':
+		elif choice == 4:
 			print("\n==========Check Balance==========\n")
-			print("There is no balance history." if not balance else balance)
-			print(f"Your balance is: R$ {balance:.2f}")
+			print("There is no balance history." if not balance else f"Your balance is: R$ {balance:.2f}")
 			print("\n=================================\n")
 
-		if choice == '5':
+		elif choice == 5:
 			print("Exit")
 			break
 		else:
 			print("Invalid choice")
-			continue
